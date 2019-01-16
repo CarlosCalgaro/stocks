c958d49793b04282055fbe806ce4c5c95b1489e5
Carlos Alberto Calgaro Filho <carlos.calgaro@keyworks.com.br>
2019-01-16 15:46:02 -0200

    Initial commit

---
+++ src/api/stocks/StocksApi.js
@@ -0,0 +1,16 @@
+
+var StocksApi = {
+    
+    API: {
+        baseUrl: 'https://api.iextrading.com/1.0/'
+    },
+
+    Stocks: {
+        byName: (name) => {
+            let url = StocksApi.API.baseUrl + 'stock/' + name + '/company';
+            return fetch(url).then(response => response.json());
+        }
+    }
+};
+
+export default StocksApi;
---
+++ src/stocks/stocks.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import StocksApi from '../api/stocks/StocksApi';
+
+
+
+class Stocks extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            stockName: "AAPL",
+        };
+    }
+
+    render(){
+        console.log(StocksApi.Stocks.byName(this.state.stockName))
+        return(
+            <div>
+                Nome da Ação: {this.state.stockName}
+            </div>
+        );
+    }
+
+}
+
+export default Stocks;
