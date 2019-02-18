fc9b8fba98f4dc334ce05bafd969dea8fcf68d2d
Carlos Calgaro <carloscalgarof@gmail.com>
2019-02-18 00:22:10 -0300

    Updates

---
+++ precache-manifest.dbab2a36a6066386e3a9a7062bd6302c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stocks/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "101fda8e334c8952a1cd",
+    "url": "/stocks/static/js/runtime~main.101fda8e.js"
+  },
+  {
+    "revision": "0fed40cea7bfab2d19b1",
+    "url": "/stocks/static/js/main.0fed40ce.chunk.js"
+  },
+  {
+    "revision": "560b0ffd05713d09f3f1",
+    "url": "/stocks/static/js/2.560b0ffd.chunk.js"
+  },
+  {
+    "revision": "0fed40cea7bfab2d19b1",
+    "url": "/stocks/static/css/main.8e3e8cbd.chunk.css"
+  },
+  {
+    "revision": "560b0ffd05713d09f3f1",
+    "url": "/stocks/static/css/2.62706fa3.chunk.css"
+  },
+  {
+    "revision": "b0daab2dab01896261219f736836fb13",
+    "url": "/stocks/index.html"
+  }
+];
